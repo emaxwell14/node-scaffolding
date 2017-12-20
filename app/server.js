b1273a75bfc4ab39becb596e708bbf760034e1d0
Eoghan Maxwell <eoghan.maxwell@cegedim-activ.com>
2017-12-20 14:32:02 +0100

    Adding docker compose and cors lib

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const router = express.Router();
 const port = 8080;
+
+app.use(cors());
 
 
 // Define middleware
